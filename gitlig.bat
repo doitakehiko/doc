9d255804b1b60dbc1372a1ee57eb9b45e9e1bdbd
unknown <takehiko.doi@gmail.com>
2019-05-15 15:33:42 +0900

    edit for error

--- gitlig.bat
+++ gitlig.bat
@@ -1,3 +1,5 @@
+if ""%1""=="""" goto End
+
 set USR_INPUT_STR=%1
 
 git add lightnovel.txt
@@ -7,6 +9,8 @@ git push origin master
 git add lightnovel_res.txt
 git commit -m %USR_INPUT_STR%
 git push origin master
+
+:End
 
 echo スクリプトを終了します．
 echo 何かキーを押してください...
