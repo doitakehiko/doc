70c8d92370bd4186f313e1b4f92a6e96fa288269
unknown <takehiko.doi@gmail.com>
2019-05-15 15:34:18 +0900

    edit for error

--- gitmanga.bat
+++ gitmanga.bat
@@ -1,3 +1,5 @@
+if ""%1""=="""" goto End
+
 set USR_INPUT_STR=%1
 
 git add manga.txt
@@ -7,6 +9,8 @@ git push origin master
 git add manga_res.txt
 git commit -m %USR_INPUT_STR%
 git push origin master
+
+:End
 
 echo スクリプトを終了します．
 echo 何かキーを押してください...
