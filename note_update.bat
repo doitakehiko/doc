3e08241d643422557609ae0141899799ddacb18e
doitakehiko <takehiko.doi@gmail.com>
2019-07-06 15:51:44 +0900

    update for notepc

---
+++ note_update.bat
@@ -0,0 +1,6 @@
+Robocopy.exe D:\doc\ F:\doc\
+ Robocopy.exe D:\manga\ F:\manga\
+Robocopy.exe D:\office\ F:\office\ /MIR
+robocopy D:\doc D:\Dropbox\Dropbox\doc   /MIR
+robocopy D:\manga D:\Dropbox\Dropbox\manga   /MIR
+robocopy D:\office D:\Dropbox\Dropbox\office  /MIR
