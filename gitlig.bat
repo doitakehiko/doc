163a1890a26792058c1f0a73ea0ce16a3b33db80
doitakehiko <takehiko.doi@gmail.com>
2019-07-08 14:49:36 +0900

    重複削除

--- gitlig.bat
+++ gitlig.bat
@@ -3,9 +3,6 @@ if ""%1""=="""" goto End
 set USR_INPUT_STR=%1
 
 git add lightnovel.txt
-git commit -m %USR_INPUT_STR%
-git push origin master
-
 git add lightnovel_res.txt
 git commit -m %USR_INPUT_STR%
 git push origin master
