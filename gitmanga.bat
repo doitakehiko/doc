3d77c581a0915ef119a341a3805091cfe8dba6b1
doitakehiko <takehiko.doi@gmail.com>
2019-07-08 14:46:09 +0900

    重複削除

--- gitmanga.bat
+++ gitmanga.bat
@@ -3,9 +3,6 @@ if ""%1""=="""" goto End
 set USR_INPUT_STR=%1
 
 git add manga.txt
-git commit -m %USR_INPUT_STR%
-git push origin master
-
 git add manga_res.txt
 git commit -m %USR_INPUT_STR%
 git push origin master
