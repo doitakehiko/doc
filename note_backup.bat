75ead1d2073fa5c93909988859f55ea72a5d844b
doitakehiko <takehiko.doi@gmail.com>
2019-08-27 14:36:53 +0900

    ログを追加

--- note_backup.bat
+++ note_backup.bat
@@ -1,20 +1,29 @@
-Robocopy D:\0721 	L:\0721 	/MIR
-Robocopy D:\c# 		L:\c# 		/MIR
-Robocopy D:\doc 	L:\doc 		/MIR
-Robocopy D:\doc_old 	L:\doc_old	/MIR
-Robocopy D:\Downloads 	L:\Downloads	/MIR
-Robocopy D:\Dropbox 	L:\Dropbox	/MIR
-Robocopy D:\java 	L:\java		/MIR
-Robocopy D:\manga 	L:\manga	/MIR
-Robocopy D:\office 	L:\office	/MIR
+set logfile="L:\log\copylog\%date:~0,4%-%date:~5,2%-%date:~8,2%_note_backup.log"
+
+Robocopy D:\0721 	L:\0721 	/LOG+:%logfile%	/MIR
+Robocopy D:\c# 		L:\c# 		/LOG+:%logfile%	/MIR
+Robocopy D:\doc 	L:\doc 		/LOG+:%logfile%	/MIR
+Robocopy D:\doc_old 	L:\doc_old	/LOG+:%logfile%	/MIR
+Robocopy D:\Downloads 	L:\Downloads	/LOG+:%logfile%	/MIR
+Robocopy D:\Dropbox 	L:\Dropbox	/LOG+:%logfile%	/MIR
+Robocopy D:\java 	L:\java		/LOG+:%logfile%	/MIR
+Robocopy D:\manga 	L:\manga	/LOG+:%logfile%	/MIR
+Robocopy D:\office 	L:\office	/LOG+:%logfile%	/MIR
 
 Robocopy F:\ChilkatDotNet4-9.4.1-win32 L:\ChilkatDotNet4-9.4.1-win32
-Robocopy F:\dlsite 	L:\dlsite	/MIR
+Robocopy F:\dlsite 	L:\dlsite	/LOG+:%logfile%	/MIR
 Robocopy F:\DotNetZipLib-Runtime-v1.9  L:\DotNetZipLib-Runtime-v1.9
-Robocopy F:\Ed 		L:\Ed		/MIR
-Robocopy F:\EvernoteBackup 	L:\EvernoteBackup 	/MIR
-Robocopy F:\EvernoteEnex 	L:\EvernoteEnex 	/MIR
-Robocopy F:\mymusic 		L:\mymusic 		/MIR
-Robocopy F:\pic_INTER 		L:\pic_INTER 		/MIR
-Robocopy F:\pic_SD 		L:\pic_SD 		/MIR
-Robocopy F:\ダウンロード 	L:\ダウンロード 	/MIR
+Robocopy F:\Ed 		L:\Ed		/LOG+:%logfile%	/MIR
+Robocopy F:\EvernoteBackup 	L:\EvernoteBackup 	/LOG+:%logfile%	/MIR
+Robocopy F:\EvernoteEnex 	L:\EvernoteEnex 	/LOG+:%logfile%	/MIR
+Robocopy F:\mymusic 		L:\mymusic 		/LOG+:%logfile%	/MIR
+Robocopy F:\pic_INTER 		L:\pic_INTER 		/LOG+:%logfile%	/MIR
+Robocopy F:\pic_SD 		L:\pic_SD 		/LOG+:%logfile%	/MIR
+Robocopy F:\ダウンロード 	L:\ダウンロード 	/LOG+:%logfile%	/MIR
+
+
+TYPE %logfile% | MORE
+
+echo スクリプトを終了します．
+echo 何かキーを押してください...
+pause >nul
