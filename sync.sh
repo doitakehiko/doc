f39121171e2551e2e8cd78980b572937182d566a
doitakehiko <takehiko.doi@gmail.com>
2020-01-25 14:26:26 +0900

    gvfs

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rsync -a -v /home/takehiko/ /media/6a4a6100-2e5e-419a-a2a1-e8a902c1899c/takehiko/   2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
-rsync -a -v /home/takehiko/ /media/ec1576a9-a507-4fee-9820-d62805dbc320/takehiko/   2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
+rsync -aAXv  --exclude={"/home/*/.gvfs"} /home/takehiko/ /media/6a4a6100-2e5e-419a-a2a1-e8a902c1899c/takehiko/   2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
+rsync -aAXv  --exclude={"/home/*/.gvfs"} /home/takehiko/ /media/f9b190f8-1faa-465b-b345-e97302a42689/takehiko/   2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
 cat /home/takehiko/log/`date "+%Y%m%d"`.err.log
