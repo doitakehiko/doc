01e43a586a819c57f131c3e390190d33f9c79320
doitakehiko <takehiko.doi@gmail.com>
2020-02-27 14:33:41 +0900

    dvd

--- note_backup.bat
+++ note_backup.bat
@@ -11,6 +11,7 @@ Robocopy D:\manga 	L:\manga	/LOG+:%logfile%	/MIR
 Robocopy D:\office 	L:\office	/LOG+:%logfile%	/MIR
 Robocopy D:\webGL 	L:\webGL	/LOG+:%logfile%	/MIR
 Robocopy D:\dbdate 	L:\dbdate	/LOG+:%logfile%	/MIR
+Robocopy D:\DVD 	L:\DVD		/LOG+:%logfile%	/MIR
 
 Robocopy F:\ChilkatDotNet4-9.4.1-win32 L:\ChilkatDotNet4-9.4.1-win32
 Robocopy F:\dlsite 		L:\dlsite		/LOG+:%logfile%	/MIR
