e5634b75e586e55d4089619c1ba26912211e0376
doitakehiko <takehiko.doi@gmail.com>
2020-05-10 13:18:32 +0900

    new pc

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rsync -aAXv  --exclude={"/home/*/.gvfs"} /home/takehiko/ /media/6a4a6100-2e5e-419a-a2a1-e8a902c1899c/takehiko/   2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
-rsync -aAXv  --exclude={"/home/*/.gvfs"} /home/takehiko/ /media/f9b190f8-1faa-465b-b345-e97302a42689/takehiko/   2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
+rsync -aAXv  /home/takehiko/ /run/media/takehiko/6a4a6100-2e5e-419a-a2a1-e8a902c1899c/takehiko/ 2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
+rsync -aAXv  /home/takehiko/ /run/media/takehiko/f9b190f8-1faa-465b-b345-e97302a42689/takehiko/ 2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
 cat /home/takehiko/log/`date "+%Y%m%d"`.err.log
