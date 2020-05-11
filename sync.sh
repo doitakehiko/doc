553559a9e504739a59da838234de62ca9eb62ca9
doitakehiko <takehiko.doi@gmail.com>
2020-05-11 09:53:20 +0900

    標準出力を追加

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rsync -aAXv  /home/takehiko/ /run/media/takehiko/6a4a6100-2e5e-419a-a2a1-e8a902c1899c/takehiko/ 2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
-rsync -aAXv  /home/takehiko/ /run/media/takehiko/f9b190f8-1faa-465b-b345-e97302a42689/takehiko/ 2>> /home/takehiko/log/`date "+%Y%m%d"`.err.log
-cat /home/takehiko/log/`date "+%Y%m%d"`.err.log
+rsync -aAXv --exclude "**/.*" /home/takehiko/ /run/media/takehiko/6a4a6100-2e5e-419a-a2a1-e8a902c1899c/takehiko/ > /home/takehiko/log/`date "+%Y%m%d"`.log 2>&1
+rsync -aAXv --exclude "**/.*"  /home/takehiko/ /run/media/takehiko/f9b190f8-1faa-465b-b345-e97302a42689/takehiko/ > /home/takehiko/log/`date "+%Y%m%d"`.log 2>&1
+cat /home/takehiko/log/`date "+%Y%m%d"`.log
