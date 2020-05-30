7d9164fe186c1b062cb59a9d6ac699f617ec0998
doitakehiko <takehiko.doi@gmail.com>
2020-05-30 14:03:27 +0900

    HDD故障

--- note_backup.bat
+++ note_backup.bat
@@ -28,35 +28,6 @@ Robocopy F:\pic_SD 		L:\pic_SD 		/LOG+:%logfile%	/MIR
 Robocopy F:\ダウンロード 	L:\ダウンロード 	/LOG+:%logfile%	/MIR
 
 
-Robocopy D:\0721 	M:\0721 	/LOG+:%logfile%	/MIR
-Robocopy D:\c# 		M:\c# 		/LOG+:%logfile%	/MIR
-Robocopy D:\doc 	M:\doc 		/LOG+:%logfile%	/MIR
-Robocopy D:\doc_old 	M:\doc_old	/LOG+:%logfile%	/MIR
-Robocopy D:\Downloads 	M:\Downloads	/LOG+:%logfile%	/MIR
-Robocopy D:\Dropbox 	M:\Dropbox	/LOG+:%logfile%	/MIR
-Robocopy D:\java 	M:\java		/LOG+:%logfile%	/MIR
-Robocopy D:\manga 	M:\manga	/LOG+:%logfile%	/MIR
-Robocopy D:\office 	M:\office	/LOG+:%logfile%	/MIR
-Robocopy D:\webGL 	M:\webGL	/LOG+:%logfile%	/MIR
-Robocopy D:\dbdate 	M:\dbdate	/LOG+:%logfile%	/MIR
-Robocopy D:\DVD 	M:\DVD		/LOG+:%logfile%	/MIR
-Robocopy "C:\Users\takeh\Pictures\Camera Roll" "M:\カメラロール" /LOG+:%logfile% /MIR
-Robocopy C:\Users\takeh\Music\iTunes 	M:\ミュージック\iTunes	/LOG+:%logfile%	/MIR
-
-
-Robocopy F:\ChilkatDotNet4-9.4.1-win32 M:\ChilkatDotNet4-9.4.1-win32
-Robocopy F:\dlsite 		M:\dlsite		/LOG+:%logfile%	/MIR
-Robocopy F:\DotNetZipLib-Runtime-v1.9  M:\DotNetZipLib-Runtime-v1.9
-Robocopy F:\Ed 			M:\Ed			/LOG+:%logfile%	/MIR
-Robocopy F:\EvernoteBackup 	M:\EvernoteBackup 	/LOG+:%logfile%	/MIR
-Robocopy F:\EvernoteEnex 	M:\EvernoteEnex 	/LOG+:%logfile%	/MIR
-Robocopy F:\mymusic 		M:\mymusic 		/LOG+:%logfile%	/MIR
-Robocopy F:\pic_INTER 		M:\pic_INTER 		/LOG+:%logfile%	/MIR
-Robocopy F:\pic_SD 		M:\pic_SD 		/LOG+:%logfile%	/MIR
-Robocopy F:\ダウンロード 	M:\ダウンロード 	/LOG+:%logfile%	/MIR
-
-
-
 TYPE %logfile% | MORE
 
 echo スクリプトを終了します．
