fd7655f640532526425205a60dc28895db50398a
doitakehiko <takehiko.doi@gmail.com>
2020-09-29 10:11:54 +0900

    ファイル名を削除

--- hon.bat
+++ hon.bat
@@ -1,3 +1,3 @@
 SET $OutputEncoding = [Console]::OutputEncoding
-find " " D:\doc\manga?.txt | D:\c#\ConsoleApplication7\ConsoleApplication7\bin\Release\ConsoleApplication7.exe | call sort > D:\doc\manga_res.txt
-find " " D:\doc\lightnovel?.txt | D:\c#\ConsoleApplication7\ConsoleApplication7\bin\Release\ConsoleApplication7.exe | call sort > D:\doc\lightnovel_res.txt
+type D:\doc\manga?.txt | find " " | D:\c#\ConsoleApplication7\ConsoleApplication7\bin\Release\ConsoleApplication7.exe | call sort  > D:\doc\manga_res.txt
+type D:\doc\lightnovel?.txt  | find " "  | D:\c#\ConsoleApplication7\ConsoleApplication7\bin\Release\ConsoleApplication7.exe | call sort > D:\doc\lightnovel_res.txt
